ddd83c2424fac11c21d055fb992e3da1197b731f
Igor Kostromin <elwood.su@gmail.com>
2015-07-07 00:17:30 +0300

    Make control flow order assertions in InnerCoroTest.

--- jcoro-app/src/test/java/org/jcoro/tests/InnerCoroTest.java
+++ jcoro-app/src/test/java/org/jcoro/tests/InnerCoroTest.java
@@ -1,5 +1,6 @@
 package org.jcoro.tests;
 
+import junit.framework.Assert;
 import org.jcoro.Coro;
 import org.jcoro.ICoroRunnable;
 import org.jcoro.Instrument;
@@ -7,11 +8,24 @@ import org.jcoro.RestorePoint;
 import org.junit.Test;
 
 /**
+ * Test for one coro creates another coro inside, and both yield and resume in correct order.
+ *
  * @author elwood
  */
 public class InnerCoroTest {
+    public static void main(String[] args) {
+        new InnerCoroTest().test();
+    }
+
+    private void assertStepIs(int step, int[] state){
+        Assert.assertEquals(step, state[0]);
+        state[0]++;
+    }
+
     @Test
     public void test() {
+        final int[] state = new int[1];
+        state[0] = 0;
         Coro outerCoro = Coro.initSuspended(new ICoroRunnable() {
             @Override
             @Instrument(@RestorePoint("yield"))
@@ -22,28 +36,46 @@ public class InnerCoroTest {
                     @Override
                     @Instrument(@RestorePoint("yield"))
                     public void run() {
+                        assertStepIs(2, state);
                         Coro _innerCoro = Coro.get();
                         int i = 5;
                         System.out.println("i = " + i);
+
                         _innerCoro.yield();
+
+                        assertStepIs(6, state);
                         i = 10;
                         System.out.println("i = " + i);
                     }
                 });
+                assertStepIs(1, state);
                 System.out.println("Outer coro started");
+
                 innerCoro.start();
+
+                assertStepIs(3, state);
                 System.out.println("Yielding from outer coro..");
 
                 _outerCoro.yield();
 
+                assertStepIs(5, state);
                 System.out.println("Resuming inner coro..");
+
                 innerCoro.resume();
+
+                assertStepIs(7, state);
                 System.out.println("Returning from outer coro");
             }
         });
+        assertStepIs(0, state);
         outerCoro.start();
+
+        assertStepIs(4, state);
         System.out.println("Resuming outer coro");
+
         outerCoro.resume();
+
+        assertStepIs(8, state);
         System.out.println("All finished");
     }
 }
